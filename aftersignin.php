493a24cbbe691c88b4324f541c45a670cec9be8d
Sunjeev Gururangan <sunjeevg@sunjeevs-air.home>
2017-12-12 23:02:18 -0500

    formatting and styles

--- aftersignin.php
+++ aftersignin.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 $con=mysqli_connect('classroom.cs.unc.edu','sgamage','finalproject','sgamagedb');
@@ -13,43 +14,43 @@ $answer = $con->query($whoToShow);
 
  $usertype = $answer->fetch_assoc();
 
+ if($usertype['Privileges']=='tutor'){
+   $result = "<a class = 'btn btn-lg btn-primary .col-md-3' href='geocode.php'>Find Students!</a>";
+ }
+ if($usertype['Privileges'] =='student'){
+   $result = "<a class = 'btn btn-lg btn-primary .col-md-3' href='geocode.php'>Find Tutors!</a>";
+ }
 
-
-
-echo "<h1 style='color:#16A085;font-family:Verdana;'>"."Hello ".$name."!"."</h1>";
-
-
-
-if($usertype['Privileges']=='tutor'){
-	echo"<a href='geocode.php'><button>Find Students!</button></a> ";
-}
-if($usertype['Privileges'] =='student'){
-	echo"<a href='geocode.php'><button>Find Tutors!</button></a> ";
-}
-
-echo "<a href='messages.php'><button>View Messaging History</button></a></br>";
-echo "<a href='index.php'>Log out</a></br>";
-
-
-
-
-
+ $view =  "<a href='messages.php'><button>View Messaging History</button></a></br>";
 
 ?>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  <link href="jumbotron-narrow.css" rel="stylesheet">
+</head>
 
-<html>
-  <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="index.css">
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-  </head>
-<body>
+  <body>
+    <div class="container">
+      <header class="header clearfix">
+      <nav>
+        <ul class="nav nav-pills float-right">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Logout<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </nav>
+      <h3 class="text-muted"><?php echo $name. "'s Profile"?></h3>
+    </header>
 
+        <div class="jumbotron">
+          <h1>Find Tutors in Your Area</h1>
+          <p class="lead"></p>
+          <?php echo $result; ?>
+        </div>
 
+      </div> <!-- /container -->
 
-
- </body>
-
-</html>
+  </body>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
   <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="index.css">
+    <link rel= "stylesheet" href = "index.css">
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
@@ -33,59 +33,42 @@
       </form>
 
         <a href="signup.php" class="">Don't have an account? Sign up here!</a>
-
-    </div>
-
-  </body>
-
-</html>
-
-
-
+      </div>
 <?php
 session_start();
 $con=mysqli_connect('classroom.cs.unc.edu','sgamage','finalproject','sgamagedb');
 
-if(isset($_POST['login'])){ 
+if(isset($_POST['login'])){
   //text was entered into form fields
+  $email=$_POST['email'];
+  $pass= $_POST['password'];
+  $error="";
+  $_SESSION['email']=$email;
 
+  $sql="SELECT * FROM Users WHERE Username='$email'";
+  $result=$con->query($sql);
+  if($result->num_rows==0){
+    echo("<span class ='incorrect' style ='color:red;'>The email and password you entered do not correspond to those in our records. Please try again.</span>");
+  }else{
+    $correctpass=$result->fetch_assoc();
+    if($pass != $correctpass['Password']){
+      echo("<span class ='incorrect' style='color:red;'>The password you entered is incorrect.</span>");
 
-$email=$_POST['email'];
-$pass= $_POST['password'];
-$error="";
-$_SESSION['email']=$email;
-
-$sql="SELECT * FROM Users WHERE Username='$email'";
-$result=$con->query($sql); 
-if($result->num_rows==0){
-  echo("<span class ='incorrect' style ='color:red;'>The email and password you entered do not correspond to those in our records. Please try again.</span>");
-}else{
-$correctpass=$result->fetch_assoc();
-if($pass != $correctpass['Password']){
-  echo("<span class ='incorrect' style='color:red;'>The password you entered is incorrect.</span>");
-  
-}else{
+    }else{
 
   ?>
-  <script>
-   window.location='aftersignin.php';
-  </script>
-    <?php
-}
+    <script>
+      window.location='aftersignin.php';
+    </script>
+
+  <?php
+  }
 
 
-}
+  }
 
 
-}
-
-
+  }
 ?>
-
-
-
-
-
-
-
-
+</body>
+</html>
